aa735641754aa5536bed19ffe2b9321efd705d8f
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-15 17:48:17 +0100

    bonnie: install manpage; homepage fixed

--- bonnie.rb
+++ bonnie.rb
@@ -1,11 +1,15 @@
 class Bonnie < Formula
   desc "C-language benchmark measuring Unix filesystems perf"
-  homepage "https://code.google.com/p/bonnie-64/"
+  homepage "http://www.textuality.com/bonnie/"
+  url "http://www.textuality.com/bonnie/bonnie.tar.gz"
+  version "2.0.6"
+  sha256 "e420ca9275ddd9aa5fdb27d86851a57eea5ff94e8ccf6ba71352d35546842937"
   head "http://bonnie-64.googlecode.com/svn/trunk/"
 
   def install
     system "make"
     bin.install "Bonnie" => "bonnie"
+    man1.install "bonnie.1"
   end
 
   test do
