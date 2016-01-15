80eb1b538c8619d3c5023bfa663520138a1c7d2f
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-15 17:45:02 +0100

    bonnie: test & desc added

--- bonnie.rb
+++ bonnie.rb
@@ -1,9 +1,14 @@
 class Bonnie < Formula
+  desc "C-language benchmark measuring Unix filesystems perf"
   homepage "https://code.google.com/p/bonnie-64/"
   head "http://bonnie-64.googlecode.com/svn/trunk/"
 
   def install
     system "make"
     bin.install "Bonnie" => "bonnie"
+  end
+
+  test do
+    system "#{bin}/bonnie"
   end
 end
